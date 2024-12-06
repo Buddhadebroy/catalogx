77bdf69c90de09d24344d4b5df2a29527e3593ec
Promita Ghosh <118420070+user-promi@users.noreply.github.com>
2024-12-06 10:51:33 +0530

    build added

--- build/blocks/enquiryForm/index.asset.php
+++ build/blocks/enquiryForm/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '9ec6fd7bf938d73c54b3');
