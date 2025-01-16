9a6ccc1bc11684b5d99c26be04be0ebb612d3fe6
Promita Ghosh <118420070+user-promi@users.noreply.github.com>
2025-01-16 17:36:36 +0530

    update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '99070c4546f464f2499e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'e7f9711117cf8604e3da');
