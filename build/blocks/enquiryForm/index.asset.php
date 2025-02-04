3272fa8fb8c1686208a98adea902276de6a8f4a1
Buddhadebroy <roybuddhadeb2003@gmail.com>
2025-02-04 16:12:20 +0530

    css update

--- build/blocks/enquiryForm/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '00890fe2117bd581d2be');
--- build/blocks/quote-thank-you/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '03c24f64996714483811');
