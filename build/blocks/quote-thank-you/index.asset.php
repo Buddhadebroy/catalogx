50307ec1738679054893c93135dc6db8c0d63724
Buddhadebroy <roybuddhadeb2003@gmail.com>
2025-02-14 12:29:06 +0530

    update

--- build/blocks/quote-thank-you/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '52464e5f3c1d6a5cfee2');
--- src/components/AdminLibrary/Inputs/Special/InputMailchimpList.jsx
+++ src/components/AdminLibrary/Inputs/Special/InputMailchimpList.jsx
@@ -7,7 +7,7 @@ import { getApiLink, getApiResponse } from "../../../../services/apiService";
 
 import { useSetting } from "../../../../contexts/SettingContext";
 
-import "./InputMailchimpList.scss";
+// import "./InputMailchimpList.scss";
 
 const InputMailchimpList = (props) => {
   const { mailchimpKey, optionKey, settingChanged } = props;
